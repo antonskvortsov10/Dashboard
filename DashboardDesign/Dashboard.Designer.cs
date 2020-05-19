d7b2c8dfb2bdc2ccc468429492d0967242df81b7
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2020-05-20 00:38:45 +0300

    Добавил изменения

--- DashboardDesign/Dashboard.Designer.cs
+++ DashboardDesign/Dashboard.Designer.cs
@@ -30,14 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.NotificationButton = new System.Windows.Forms.Button();
+            this.ContactsButton = new System.Windows.Forms.Button();
+            this.UserButton = new System.Windows.Forms.Button();
             this.MenuButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.UserButton = new System.Windows.Forms.Button();
-            this.ContactsButton = new System.Windows.Forms.Button();
-            this.NotificationButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -53,6 +63,42 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1203, 49);
             this.panel1.TabIndex = 0;
+            // 
+            // NotificationButton
+            // 
+            this.NotificationButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("NotificationButton.BackgroundImage")));
+            this.NotificationButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.NotificationButton.FlatAppearance.BorderSize = 0;
+            this.NotificationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NotificationButton.Location = new System.Drawing.Point(1040, 5);
+            this.NotificationButton.Name = "NotificationButton";
+            this.NotificationButton.Size = new System.Drawing.Size(46, 41);
+            this.NotificationButton.TabIndex = 4;
+            this.NotificationButton.UseVisualStyleBackColor = true;
+            // 
+            // ContactsButton
+            // 
+            this.ContactsButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ContactsButton.BackgroundImage")));
+            this.ContactsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ContactsButton.FlatAppearance.BorderSize = 0;
+            this.ContactsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ContactsButton.Location = new System.Drawing.Point(1092, 5);
+            this.ContactsButton.Name = "ContactsButton";
+            this.ContactsButton.Size = new System.Drawing.Size(52, 41);
+            this.ContactsButton.TabIndex = 3;
+            this.ContactsButton.UseVisualStyleBackColor = true;
+            // 
+            // UserButton
+            // 
+            this.UserButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("UserButton.BackgroundImage")));
+            this.UserButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.UserButton.FlatAppearance.BorderSize = 0;
+            this.UserButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UserButton.Location = new System.Drawing.Point(1150, 5);
+            this.UserButton.Name = "UserButton";
+            this.UserButton.Size = new System.Drawing.Size(41, 41);
+            this.UserButton.TabIndex = 2;
+            this.UserButton.UseVisualStyleBackColor = true;
             // 
             // MenuButton
             // 
@@ -82,6 +128,10 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Snow;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 49);
             this.panel2.Name = "panel2";
@@ -91,47 +141,83 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(217, 49);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(986, 42);
             this.panel3.TabIndex = 2;
             // 
-            // UserButton
+            // label2
             // 
-            this.UserButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("UserButton.BackgroundImage")));
-            this.UserButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.UserButton.FlatAppearance.BorderSize = 0;
-            this.UserButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.UserButton.Location = new System.Drawing.Point(1150, 5);
-            this.UserButton.Name = "UserButton";
-            this.UserButton.Size = new System.Drawing.Size(41, 41);
-            this.UserButton.TabIndex = 2;
-            this.UserButton.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dashboard";
             // 
-            // ContactsButton
+            // label3
             // 
-            this.ContactsButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ContactsButton.BackgroundImage")));
-            this.ContactsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ContactsButton.FlatAppearance.BorderSize = 0;
-            this.ContactsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ContactsButton.Location = new System.Drawing.Point(1092, 5);
-            this.ContactsButton.Name = "ContactsButton";
-            this.ContactsButton.Size = new System.Drawing.Size(52, 41);
-            this.ContactsButton.TabIndex = 3;
-            this.ContactsButton.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(839, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Home / Dashboard";
             // 
-            // NotificationButton
+            // pictureBox1
             // 
-            this.NotificationButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("NotificationButton.BackgroundImage")));
-            this.NotificationButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.NotificationButton.FlatAppearance.BorderSize = 0;
-            this.NotificationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.NotificationButton.Location = new System.Drawing.Point(1040, 5);
-            this.NotificationButton.Name = "NotificationButton";
-            this.NotificationButton.Size = new System.Drawing.Size(46, 41);
-            this.NotificationButton.TabIndex = 4;
-            this.NotificationButton.UseVisualStyleBackColor = true;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(58, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Dashboard";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(58, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Chart";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
             // 
             // Dashboard
             // 
@@ -148,6 +234,11 @@
             this.Text = "Dashboard";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -162,5 +253,11 @@
         private System.Windows.Forms.Button UserButton;
         private System.Windows.Forms.Button ContactsButton;
         private System.Windows.Forms.Button NotificationButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
